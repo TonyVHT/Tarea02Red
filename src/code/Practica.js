6a6fe37603a0d55ea7fc6a8c43ed3910f9deee32
TonyVHT <tvillegashu@gmail.com>
2025-02-27 20:22:34 -0600

    feat: add preference function

--- src/code/Practica.js
+++ src/code/Practica.js
@@ -2,21 +2,25 @@ const express = require('express');
 const crypto = require('crypto');
 const app = express();
 const config = require('../config/Config');
+
 const PORT = config.NODE_PORT_DES_RED;
+const APPLICATION_JSON = 'application/json';
+const APPLICATION_XML = 'application/xml';
+const TEXT_HTML = 'text/html';
 
 const helperServerResponse = (req, res, message) => {
-    const accept = req.headers.accept;
+    const acceptHeader = req.headers.accept || '';
 
-    if(accept.includes('application/json')) {
+    if(acceptHeader.includes(APPLICATION_JSON)) {
         res.json(message);
-    }else if(accept.includes('application/xml')){
+    }else if(acceptHeader.includes(APPLICATION_XML)){
         let xmlResponse = `<response>`;
         for (const key in message) {
             xmlResponse += `<${key}>${message[key]}</${key}>`;
         }
         xmlResponse += `</response>`;
         res.type('application/xml').send(xmlResponse);
-    }else if(accept.includes("text/html")){
+    }else if(acceptHeader.includes(TEXT_HTML)){
         let htmlResponse = '<html><body>';
         for (const key in message) {
             htmlResponse += `<p>${key}: ${message[key]}</p>`;
@@ -31,6 +35,25 @@ const helperServerResponse = (req, res, message) => {
 app.get('/info', (req, res) =>{
     const defaultMessage = {message: ' *Giving information... bip bup*'}
     helperServerResponse(req, res, defaultMessage);
+})
+
+app.get('/preferencia', (req, res) =>{
+    const acceptHeader = req.headers.accept;
+    const typeHeader = acceptHeader.split(',').map(format => {
+        const [type, q = 'q=1'] = format.split(';');
+        return { type: type.trim(), q: parseFloat(q.split('=')[1] || 1) };
+    }).sort((a, b) => b.q - a.q);
+
+    const supportedFormats = [APPLICATION_JSON, APPLICATION_XML, TEXT_HTML];
+    const matchHeader = typeHeader.find(header => supportedFormats.includes(header.type));
+
+    if(matchHeader){
+        const message = {message: ` *Preferred format: ${matchHeader.type}*`}
+        helperServerResponse(req, res, message);
+    }
+    else{
+        res.status(406).send('Not Acceptable');
+    }
 })
 
 app.listen(PORT, () => {
