3ce8ef4fff5973f076e03ca0203ce9cc96b3ee24
TonyVHT <tvillegashu@gmail.com>
2025-02-27 23:00:48 -0600

    fix: fix bug with format responses. feat: add function to know format from header

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # HTTP - Negociación de Contenido, Caching, Etag :books:
 Aplicar los conceptos de negociación de contenido, Etag y control de caché en un servicio RESTful utilizando Node.js y Express.
 
-## Endpoint de Negociación de Contenido 
+## Endpoint de Negociación de Contenido **/info**
 Dependiendo del tipo de petición que se realice (contenida en el accept del header), el servidor responderá con un tipo de contenido diferente. En este caso las opciones de respuesta son: json, xml y html.
 Para ello, nos servimos de un método de apoyo que se encarga de verificar si el accept del header contiene alguno de los formatos anteriormente mencionados. 
 
@@ -12,8 +12,18 @@ Para probar que el endpoint funciona correctamente, dentro de Postman podemos ha
 Colocar el URL con el formato http://direccion:puerto/info. Además, tendremos que modificar el header de postman, para que el accept lleve el tipo de petición que se está realizando.
 
 ![alt text](images/image02.png)
+![alt text](images/image03.png)
+![alt text](images/image04.png)
+Como vemos, la respuesta la da en el formato solicitado.
 
+## Endpoint de preferencia **/preferencia**
 
+En este caso, el servidor responderá con el formato que tenga mayor prioridad en el accept del header. En caso de que no se encuentre ninguno de los formatos soportados, se responderá con un error 406.
+Para probar que el endpoint funciona correctamente, dentro de Postman podemos hacer lo siguiente.
+
+![alt text](images/image05.png)
+
+Dentro del valor del accept en el header, agregaremos los niveles de preferencia (q) a cada uno de los formatos, de modo que el de mayor valor sea el que se responda. Por ejemplo, en la imagen de arriba podemos observar que la respuesta se hizo en JSON. Sin embargo, en la siguiente imagen veremos que se hizo en XML. 
 ----
 Alumno: Tony Villegas Hurtado 
 EE: DESARROLLO DE SISTEMAS EN RED
--- src/code/Practica.js
+++ src/code/Practica.js
@@ -18,34 +18,7 @@ const MESSAGE_ERROR_406 = 'Not Acceptable';
 const MESSAGE_CACHE_RESPONSE = 'Respuesta con control de caché';
 const MESSAGE_RESPONSE_ETAG = 'Contenido con ETAG';
 
-
-const helperServerResponse = (req, res, message) => {
-    const acceptHeader = req.headers.accept || '';
-
-    const messageContent = message.message || message; 
-
-    if (acceptHeader.includes(APPLICATION_JSON)) {
-        res.json(message);
-    } else if (acceptHeader.includes(APPLICATION_XML)) {
-        let xmlResponse = 
-            `<response><message>${typeof messageContent === 'object' 
-            ? JSON.stringify(messageContent) : messageContent}</message></response>`;
-            res.type(APPLICATION_XML).send(xmlResponse);
-    } else if (acceptHeader.includes(TEXT_HTML)) {
-        let htmlResponse = `<html><body><p>${messageContent}</p></body></html>`;
-        res.type(TEXT_HTML).send(htmlResponse);
-    } else {
-        res.status(406).send(MESSAGE_ERROR_406);
-    }
-}
-
-app.get(URL_INFO, (req, res) =>{
-    const defaultMessage = {message: MESSAGE_RESPONSE_INFO}
-    helperServerResponse(req, res, defaultMessage);
-})
-
-app.get(URL_PREFERENCIA, (req, res) =>{
-    const acceptHeader = req.headers.accept || '';
+const getPreferredFormat = (acceptHeader) => {
     const typeHeader = acceptHeader.split(',').map(format => {
         const [type, q = 'q=1'] = format.split(';');
         return { type: type.trim(), q: parseFloat(q.split('=')[1] || 1) };
@@ -54,18 +27,50 @@ app.get(URL_PREFERENCIA, (req, res) =>{
     const supportedFormats = [APPLICATION_JSON, APPLICATION_XML, TEXT_HTML];
     const matchHeader = typeHeader.find(header => supportedFormats.includes(header.type));
 
-    if(matchHeader){
-        const message = {message: ` *Preferred format: ${matchHeader.type}*`}
-        helperServerResponse(req, res, message);
-    }
-    else{
+    return matchHeader ? matchHeader.type : null;
+};
+
+const helperServerResponse = (res, message, preferredFormat) => {
+    const messageContent = message.message || message; 
+
+    if (preferredFormat === APPLICATION_JSON) {
+        res.json(message);
+    } else if (preferredFormat === APPLICATION_XML) {
+        let xmlResponse = `<response><message>${typeof messageContent === 'object' 
+            ? JSON.stringify(messageContent) : messageContent}</message></response>`;
+        res.type(APPLICATION_XML).send(xmlResponse);
+    } else if (preferredFormat === TEXT_HTML) {
+        let htmlResponse = `<html><body><p>${messageContent}</p></body></html>`;
+        res.type(TEXT_HTML).send(htmlResponse);
+    } else {
         res.status(406).send(MESSAGE_ERROR_406);
     }
-})
+};
+
+app.get(URL_INFO, (req, res) => {
+    const defaultMessage = { message: MESSAGE_RESPONSE_INFO };
+    const acceptHeader = req.headers.accept || '';
+    const preferredFormat = getPreferredFormat(acceptHeader);
+    helperServerResponse(res, defaultMessage, preferredFormat);
+});
+
+app.get(URL_PREFERENCIA, (req, res) => {
+    const acceptHeader = req.headers.accept || '';
+    const preferredFormat = getPreferredFormat(acceptHeader);
+
+    if (preferredFormat) {
+        const message = { message: ` *Preferred format: ${preferredFormat}*` };
+        helperServerResponse(res, message, preferredFormat);
+    } else {
+        res.status(406).send(MESSAGE_ERROR_406);
+    }
+});
 
 app.get(URL_CACHE, (req, res) => {
-    res.set({'Cache-Control': 'max-age=30','Expires': 
-        new Date(Date.now() + 30000).toUTCString(),'Pragma': 'no-cache'
+    res.set({
+        'Cache-Control': 'max-age=30',
+        'Expires': new Date(Date.now() + 30000).toUTCString(),
+        'Pragma': 'no-cache'
     });
     res.send(MESSAGE_CACHE_RESPONSE);
 });
@@ -80,7 +85,6 @@ app.get(URL_ETAG, (req, res) => {
         res.send(MESSAGE_RESPONSE_ETAG);
     }
 });
-
 
 app.listen(PORT, () => {
     console.log(`Try http://localhost:${PORT} to send a request`);
